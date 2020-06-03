f289e046f11e502d47eee02736ab0e1f8d33138d
adwid <adrien.widart@student.uclouvain.be>
2020-06-03 09:38:54 +0200

    [Q][FIX] actorHandler

--- Querier/handlers/actorHandler.js
+++ Querier/handlers/actorHandler.js
@@ -17,7 +17,7 @@ function getInboxAddresses(userIDs) {
 
 function convertAddress(addr) {
     let regExp = /https?:\/\/([0-9]{1,3}\.){3,3}[0-9]:[0-9]+(\/inbox)?\/([A-Z]*[a-z]*[0-9]*)+/gi;
-    let url = addr.match(regExp) ? process.env.PREFIX + process.env.HOST + ":" + process.env.CARPOOLING_INBOX_PORT : recipient.data.inbox;
+    let url = addr.match(regExp) ? process.env.PREFIX + process.env.HOST + ":" + process.env.CARPOOLING_INBOX_PORT : addr;
     return url + "/carpooling/news"
 }
 
